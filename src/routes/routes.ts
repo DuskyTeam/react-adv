519d1f22e9335edbc00cf1e4566244d646ae3749
DanielP <duskvillainn@gmail.com>
2022-12-04 16:39:13 -0500

    Rutas anidadas Listas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,6 @@
 import { lazy, LazyExoticComponent } from "react";
 //import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+import { NoLazy } from "../01-lazyload/pages/NoLazy";
 
 type JSXComponent = () => JSX.Element;
 interface Route {
@@ -9,42 +10,44 @@ interface Route {
   name: string;
 }
 
-const Lazy2 = lazy(
+const LazyLayout = lazy(
   () =>
     import(
-      /* webpackChunckName: "LazyPage1" */ "../01-lazyload/pages/LazyPage2"
-    )
-);
-const Lazy1 = lazy(
-  () =>
-    import(
-      /* webpackChunckName: "LazyPage2" */ "../01-lazyload/pages/LazyPage1"
-    )
-);
-const Lazy3 = lazy(
-  () =>
-    import(
-      /* webpackChunckName: "LazyPage3" */ "../01-lazyload/pages/LazyPage3"
+      /* webpackChunckName: "LazyLayout" */ "../01-lazyload/layout/LazyLayout"
     )
 );
 
+// const Lazy2 = lazy(
+//   () =>
+//     import(
+//       /* webpackChunckName: "LazyPage1" */ "../01-lazyload/pages/LazyPage2"
+//     )
+// );
+// const Lazy1 = lazy(
+//   () =>
+//     import(
+//       /* webpackChunckName: "LazyPage2" */ "../01-lazyload/pages/LazyPage1"
+//     )
+// );
+// const Lazy3 = lazy(
+//   () =>
+//     import(
+//       /* webpackChunckName: "LazyPage3" */ "../01-lazyload/pages/LazyPage3"
+//     )
+// );
+
 export const routes: Route[] = [
   {
-    to: "/lazy1",
-    path: "lazy1",
-    Component: Lazy1,
-    name: "Lazi-1",
+    path: "/lazyload/*",
+    to: "/lazyload/",
+    Component: LazyLayout,
+    name: "Lazi Layout - Dashboard",
   },
+
   {
-    to: "/lazy2",
-    path: "lazy2",
-    Component: Lazy2,
-    name: "Lazi-2",
-  },
-  {
-    to: "/lazy3",
-    path: "lazy3",
-    Component: Lazy3,
-    name: "Lazi-3",
+    to: "/no-lazy",
+    path: "no-lazy",
+    Component: NoLazy,
+    name: "No Lazy",
   },
 ];
